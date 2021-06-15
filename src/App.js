222dce98c041b562b8e554cecd34f1996297fa0d
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-15 21:11:54 +0200

    registroPersonal done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
+import { useState } from "react";
+import { RegistroPersonalData } from "./components/RegistroPersonalData";
+
 function App() {
+  const [usuario, setUsuario] = useState({
+    nombre: "",
+    apellidos: "",
+    fechaNacimiento: "",
+    email: "",
+    usuario: "",
+    pass: "",
+  });
+
+  const registroDatosPersonales = (e, objetoDatos) => {
+    e.preventDefault();
+
+    setUsuario({
+      ...usuario,
+      nombre: objetoDatos.nombre,
+      apellidos: objetoDatos.apellidos,
+      fechaNacimiento: objetoDatos.fechaNacimiento,
+      email: objetoDatos.email,
+    });
+  };
   return (
-    <div
-      className="bg-primary vh-100 d-flex align-items-center justify-content-center container
-"
-    >
-      <form
-        className="
-        bg-light
-        cajaFormulario
-        row
-        p-3
-        rounded
-        align-items-center
-        justify-content-center
-      "
-      >
-        <h1 className="col-12 titulo text-center">Registro</h1>
-        <label className="col-6">Nombre:</label>{" "}
-        <input type="text" className="col-6" />
-        <label className="col-6">Apellidos:</label>
-        <input type="text" className="col-6" />
-        <label className="col-6">Fecha Nacimiento:</label>
-        <input type="date" className="col-6" />{" "}
-        <label className="col-6">E-mail </label>
-        <input type="email" className="col-6" />
-        <button className="col-12 btn bg-primary text-light">Registrar</button>
-      </form>
+    <div className="bg-primary vh-100 d-flex align-items-center justify-content-center containerCustom">
+      <RegistroPersonalData
+        usuario={usuario}
+        setUsuario={registroDatosPersonales}
+      />
     </div>
   );
 }
---
+++ src/components/RegistroPersonalData.js
@@ -0,0 +1,80 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export const RegistroPersonalData = (props) => {
+  // const {
+  //   usuario: { nombre, apellidos, fechaNacimiento, email },
+  //   setUsuario,
+  // } = props;
+  const { setUsuario } = props;
+
+  const [nombre, setNombre] = useState("");
+  const [apellidos, setApellidos] = useState("");
+  const [fechaNacimiento, setFechaNacimiento] = useState("");
+  const [email, setEmail] = useState("");
+
+  const objetoReturn = {
+    nombre,
+    apellidos,
+    fechaNacimiento,
+    email,
+  };
+
+  return (
+    <form
+      className="
+        bg-light
+        cajaFormulario
+        row
+        p-3
+        rounded
+        align-items-center
+        justify-content-center
+      "
+      onSubmit={(e) => setUsuario(e, objetoReturn)}
+    >
+      <h1 className="col-12 titulo text-center">Registro</h1>
+      <label className="col-6">Nombre:</label>
+      <input
+        type="text"
+        className="col-6"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+      />
+      <label className="col-6">Apellidos:</label>
+      <input
+        type="text"
+        className="col-6"
+        value={apellidos}
+        onChange={(e) => setApellidos(e.target.value)}
+      />
+      <label className="col-6">Fecha Nacimiento:</label>
+      <input
+        type="date"
+        className="col-6"
+        value={fechaNacimiento}
+        onChange={(e) => setFechaNacimiento(e.target.value)}
+      />
+      <label className="col-6">E-mail </label>
+      <input
+        type="email"
+        className="col-6"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit" className="col-12 btn bg-primary text-light">
+        Registrar
+      </button>
+    </form>
+  );
+};
+// RegistroPersonalData.propTypes = {
+//   usuario: PropTypes.shape({
+//     nombre: PropTypes.string.isRequired,
+//     apellidos: PropTypes.string.isRequired,
+//     fechaNacimiento: PropTypes.string.isRequired,
+//     email: PropTypes.string.isRequired,
+//     usuario: PropTypes.string,
+//     pass: PropTypes.string,
+//   }).isRequired,
+// };
