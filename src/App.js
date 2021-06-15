39d9b83bdd411f3cd48da86861a2f6c07323cdc1
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-15 20:38:01 +0200

    maquetacion inicial añadida

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="bg-primary vh-100 d-flex align-items-center justify-content-center container
+"
+    >
+      <form
+        className="
+        bg-light
+        cajaFormulario
+        row
+        p-3
+        rounded
+        align-items-center
+        justify-content-center
+      "
+      >
+        <h1 className="col-12 titulo text-center">Registro</h1>
+        <label className="col-6">Nombre:</label>{" "}
+        <input type="text" className="col-6" />
+        <label className="col-6">Apellidos:</label>
+        <input type="text" className="col-6" />
+        <label className="col-6">Fecha Nacimiento:</label>
+        <input type="date" className="col-6" />{" "}
+        <label className="col-6">E-mail </label>
+        <input type="email" className="col-6" />
+        <button className="col-12 btn bg-primary text-light">Registrar</button>
+      </form>
     </div>
   );
 }
